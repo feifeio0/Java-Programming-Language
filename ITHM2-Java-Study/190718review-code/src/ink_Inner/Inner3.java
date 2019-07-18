708cdd7dc48c2e0513f4da9324be12b7f107260c
韩非 <40054449+feifeio0@users.noreply.github.com>
2019-07-18 18:44:15 +0800

    Update Inner3.java

--- ITHM2-Java-Study/190718review-code/src/ink_Inner/Inner3.java
+++ ITHM2-Java-Study/190718review-code/src/ink_Inner/Inner3.java
@@ -2,7 +2,7 @@ package ink_Inner;
 
 public class Inner3 {
     public static void main(String[] args) {
-        Outer obj = new Outer();
+        Outer3 obj = new Outer3();
         obj.methodOuter();
     }
 }
