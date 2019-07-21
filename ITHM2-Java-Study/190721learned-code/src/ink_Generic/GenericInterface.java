138c31d31437c745f3381962906cc1f0fd0f7b9e
韩非 <40054449+feifeio0@users.noreply.github.com>
2019-07-21 21:05:51 +0800

    learned

---
+++ ITHM2-Java-Study/190721learned-code/src/ink_Generic/GenericInterface.java
@@ -0,0 +1,7 @@
+package ink_Generic;
+/*
+    定义含有泛型的接口
+ */
+public interface GenericInterface<I> {
+    public abstract void method(I i);
+}
---
+++ ITHM2-Java-Study/190721learned-code/src/ink_Generic/GenericMethod03.java
@@ -0,0 +1,24 @@
+package ink_Generic;
+/*
+    测试含有泛型的方法
+ */
+public class GenericMethod03 {
+    public static void main(String[] args) {
+        //创建GenericMethod对象
+        GenericMethod gm = new GenericMethod();
+        /*
+            调用含有泛型的方法method01
+            传递什么类型,泛型就是什么类型
+         */
+        gm.method01(10);
+        gm.method01("abc");
+        gm.method01(8.8);
+        gm.method01(true);
+
+        gm.method02("静态方法,不建议创建对象使用");
+        //静态方法,通过类名.方法名(参数)可以直接使用
+        GenericMethod.method02("静态方法");
+        GenericMethod.method02(1);
+    }
+}
+
